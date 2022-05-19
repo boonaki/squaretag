ddace1767b60f9055389901cc6e769e231673f82
boonaki <91851563+boonaki@users.noreply.github.com>
2022-05-19 00:28:08 -0400

    fixed bugs with playagain and click count

--- js/main.js
+++ js/main.js
@@ -3,35 +3,24 @@ let start = document.querySelector(".start")
 start.addEventListener("click", startGame)
 
 //creates delay for timeout
-let delay = 280;
+let delay = 230;
 
 //creates new array obj
 const arrOfBoxes = new ArrofBoxes()
 
+//creates playAgain constant for ease of access
 const playAgain = document.querySelector('#playAgain')
 playAgain.addEventListener("click", resetGame)
 
+//declares count variables
 let clickerCount = document.querySelector('#clickCount')
-
 let count = 0
 
+//variables declared related to winning
+let gameWon = false
+// let winCount =  0
+// let winElement = document.querySelector('#winCount')
 
-function startGame(){ //starts game
-    start.classList.add('hidden')
-    let theGame = document.querySelector('.game')
-    theGame.classList.toggle('hidden')
-    theGame.addEventListener("click", countClicks)
-    findNewSec()
-}
-
-function countClicks(){
-    count += 1
-    clickerCount.innerHTML = count
-}
-
-function resetGame(){ //resets game
-    start.classList.remove('hidden')
-}
 
 function ArrofBoxes(){ //object for boxes
     const boxes = document.querySelectorAll('.box')
@@ -43,6 +32,38 @@ function ArrofBoxes(){ //object for boxes
         return this.arr[Math.floor(Math.random() * this.arr.length)];
     }
 }
+
+function startGame(){ //starts game
+    start.classList.add('hidden')
+    let theGame = document.querySelector('.game')
+    theGame.classList.toggle('hidden')
+    theGame.addEventListener("click", countClicks)
+    findNewSec()
+}
+
+function countClicks(){ //will count clicks until game won
+    if(!gameWon){
+        count += 1
+        clickerCount.innerHTML = count
+    }
+}
+
+// function countWins(){
+//     winCount += 1
+//     winElement.innerHTML = winCount
+// }
+
+function resetGame(){ //resets game
+    start.classList.remove('hidden')
+    document.querySelector('.game').classList.toggle('hidden')
+    arrOfBoxes.arr.forEach((element) => {
+        element.classList.remove('visibility')
+        element.classList.add('visibility')
+    })
+    count = 0
+    clickerCount.innerHTML = count
+}
+
 
 function findNewSec(){ //finds random section to be tagged
     let randSec = arrOfBoxes.grabBox()
@@ -67,9 +88,10 @@ function reset(mouseenter){ //resets on hover
 function green(click){ //winning function
     click.target.classList.add('greenBack')
     click.target.classList.remove('visibility')
-    winningAnimation();
+    gameWon = true
+    winningAnimation()
 }
 
 function winningAnimation(){
-
+    alert('you won!')
 }
